3cc5e9897fad72b22017db261100bec70261ce6b
Wallacy Pasqualini <wallacypasqualini@gmail.com>
2025-12-03 16:18:32 -0500

    Added cursor animation to Kitty and configured NeoVim for better integration with Kitty

--- .config/nvim/lua/plugins/animation.lua
+++ .config/nvim/lua/plugins/animation.lua
@@ -1,7 +1,12 @@
 return {
   "sphamba/smear-cursor.nvim",
   event = "VeryLazy",
-  config = function()
-    require("smear_cursor").enabled = true
-  end,
+  opts = {
+    enabled = true,
+    smear_between_buffers = false,
+    stiffness = 0.9,
+    trailing_stiffness = 0.8,
+    distance_stop_animating = 0.3,
+  },
 }
+
