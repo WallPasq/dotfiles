ab70982a77a034ac9bed619d3a64888c2036dd57
Wallacy Pasqualini <wallacypasqualini@gmail.com>
2025-12-04 19:52:56 -0500

    Added and reorganized Neovim plugins

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,16 +1,14 @@
 -- Base Editor Behavior
 
-vim.g.mapleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = "a"
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.autoindent = true
 vim.opt.cursorline = true
 vim.opt.splitright = true
-vim.opt.splitbelow = trye
+vim.opt.splitbelow = true
 vim.opt.swapfile = false
 vim.opt.clipboard = "unnamedplus"
 vim.opt.breakindent = true
